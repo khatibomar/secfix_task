7049eac937ab192c7f8077125b5ab6e2cbcd6ec6
khatibomar <elkhatibomar@outlook.com>
2025-04-02 10:45:11 +0300

    add OS, OSQuery version & app listing
    Replace simple computer name query with more detailed system information
    output. Now displays:
    - Operating system version
    - OSQuery version
    - List of installed applications
    
    This provides more comprehensive system information for diagnostics and
    system auditing purposes.

--- cmd/osquery/app.go
+++ cmd/osquery/app.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/osquery/osquery-go"
 )
+
+const appsQuery = "SELECT name FROM apps;"
+const osVersionQuery = "SELECT version FROM os_version;"
+const osQueryVersionQuery = "SELECT version FROM osquery_info;"
 
 type Application struct {
 	OSQueryClient *osquery.ExtensionManagerClient
@@ -18,16 +23,44 @@ func NewApplication(osQueryClient *osquery.ExtensionManagerClient) *Application 
 }
 
 func (app *Application) Run(ctx context.Context) error {
-	query := "SELECT computer_name FROM system_info;"
-	resp, err := app.OSQueryClient.QueryContext(ctx, query)
+	var osVersion string
+	var osqueryVersion string
+
+	resp, err := app.OSQueryClient.QueryRowContext(ctx, osVersionQuery)
 	if err != nil {
-		return fmt.Errorf("failed to query system info: %w", err)
+		return fmt.Errorf("failed to get OS version: %w", err)
 	}
 
-	fmt.Println("Query Results:")
-	for _, row := range resp.Response {
-		fmt.Println(row)
+	osVersion = resp["version"]
+
+	resp, err = app.OSQueryClient.QueryRowContext(ctx, osQueryVersionQuery)
+	if err != nil {
+		return fmt.Errorf("failed to get OSQuery version: %w", err)
 	}
+	osqueryVersion = resp["version"]
+
+	appsQueryResp, err := app.OSQueryClient.QueryRowsContext(ctx, appsQuery)
+	if err != nil {
+		return fmt.Errorf("failed to get installed apps: %w", err)
+	}
+
+	appNames := make([]string, len(appsQueryResp))
+	fmt.Println("Query Results:")
+	for i, row := range appsQueryResp {
+		appNames[i] = row["name"]
+	}
+
+	var sb strings.Builder
+	sb.Grow(1024)
+	for _, n := range appNames {
+		sb.WriteString(n)
+		sb.WriteRune('\n')
+	}
+
+	fmt.Printf("OS version: %s\n", osVersion)
+	fmt.Printf("OSQuery version: %s\n", osqueryVersion)
+	fmt.Println("Installed apps:")
+	fmt.Print(sb.String())
 
 	return nil
 }
